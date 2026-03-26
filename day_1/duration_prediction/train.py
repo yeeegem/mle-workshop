a8e7dc6adcb774fbc430f0681eb14c7f472d7e0d
Vasili Areshka <yeeegem@users.noreply.github.com>
2026-03-26 09:36:09 +0000

    add train py

---
+++ day_1/duration_prediction/train.py
@@ -0,0 +1,62 @@
+
+import pandas as pd
+import pickle
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+def read_dataframe(filename):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
+
+
+df_train = read_dataframe('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet')
+df_val = read_dataframe('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-02.parquet')
+
+print(len(df_train), len(df_val))
+
+categorical = ['PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+dv = DictVectorizer()
+
+train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+X_train = dv.fit_transform(train_dicts)
+
+val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+X_val = dv.transform(val_dicts)
+
+
+
+
+target = 'duration'
+y_train = df_train[target].values
+y_val = df_val[target].values
+
+
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+print(mean_squared_error(y_val, y_pred, squared=False) )
+
+
+with open('lin_reg.bin', 'wb') as f_out:
+    pickle.dump((dv, lr), f_out)
+
+
+
+
+
